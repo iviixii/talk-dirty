9f68a471dc726af3497e4c5dbfa7fa22f43ec447
madgrid <aaronmadved@gmail.com>
2019-05-19 19:43:32 +0200

    configuration variables for the chatkit api

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/aa94be81-49fe-4f1f-8513-acb048d7ec51/token";
+const instanceLocator = "v1:us1:aa94be81-49fe-4f1f-8513-acb048d7ec51";
+
+export { tokenUrl, instanceLocator }
